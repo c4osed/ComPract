5fdbd7d3102ebfc0d863f87e2217917efede12a1
tlekmetta <tlekmetta@gmail.com>
2017-09-13 22:20:33 +0700

    Add logo animation at login screen and done with reset password

--- app/src/main/java/com/zoazh/le/ComPract/controller/sub/ResetPasswordActivity.java
+++ app/src/main/java/com/zoazh/le/ComPract/controller/sub/ResetPasswordActivity.java
@@ -65,7 +65,7 @@ public class ResetPasswordActivity extends AppCompatActivity {
                                     Toast.makeText(ResetPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
 
                                 } else {
-                                    Toast.makeText(ResetPasswordActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(ResetPasswordActivity.this, "We couldn't find your account with that email!", Toast.LENGTH_SHORT).show();
 
                                 }
                             }
